f018443656756302d60206978bbae061fee05725
jasuriy <jasur1998gopirjonov@gmail.com>
2020-07-19 13:03:27 +0500

    Updates

---
+++ precache-manifest.6159a03030946df0c8c32958563abf9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b73a10576bb31ae42918aa115d468c",
+    "url": "/React-in-github-demo/index.html"
+  },
+  {
+    "revision": "8e0382c6b5ba63367f73",
+    "url": "/React-in-github-demo/static/css/main.7064b5b8.chunk.css"
+  },
+  {
+    "revision": "8d931342ee447c4e4b46",
+    "url": "/React-in-github-demo/static/js/2.f9800fab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-in-github-demo/static/js/2.f9800fab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0382c6b5ba63367f73",
+    "url": "/React-in-github-demo/static/js/main.007764d1.chunk.js"
+  },
+  {
+    "revision": "6ecd9afbf626903917a4",
+    "url": "/React-in-github-demo/static/js/runtime-main.ee5b3188.js"
+  }
+]);
